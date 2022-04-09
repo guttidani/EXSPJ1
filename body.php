15bef407ff0a5111a81237577fa687af3f72fc49
guttidani <61143051+guttidani@users.noreply.github.com>
2022-04-09 21:28:19 +0200

    előrőlkezdeni
    dropdown furan működik

--- body.php
+++ body.php
@@ -5,7 +5,11 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
+    <script>
+        src = "js/bootstrap.js"
+    </script>
 </head>
 
 <body>
@@ -14,8 +18,22 @@
             <div class="col mid">SZia
                 <!-- bal -->
             </div>
-            <div class="col mid">szia
+            <div class="col-6 mid">szia
                 <!-- közepso -->
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item active" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </div>
             </div>
             <div class="col mid">szs
                 <!-- jobb -->
--- createWorker.php
+++ createWorker.php
@@ -7,38 +7,133 @@ include "header.php"
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
+    <script>
+        src = "js/bootstrap.js"
+    </script>
+
     <title>Dolgozó felvétele</title>
 </head>
 
+
+
 <body>
 
-    <body>
-        <div class="container-l">
-            <div class="row">
-                <div class="container-xl col mid">
-                    <div class="container-xl col">
-                        <!-- közepso -->
-                        <div>
-                            <!-- input form -->
-                            <!-- ide kell a kozepso div címe, kozepre igazítva-->
-                            <div class=" input-group input-group-sm mb-3 mid inputField">
-                                <span class="input-group-text" id="inputGroup-sizing-s">Vezetéknév</span> <!-- ID ne felejtsd -->
-                                <input type="text" class="form-control mid" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-s">
-                            </div>
-                            <div class="input-group input-group-sm mb-3 inputField">
-                                <span class="input-group-text" id="inputGroup-sizing-sm">Keresztnév</span>
-                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-                            </div>
-                            <button id="createWorkerBtn" type="button" class="btn btn-primary">Mentés</button>
+    <div class="container-l">
+        <div class="row">
+            <div class="container-xl col">
+                <div class="container-xl">
+                    <!-- közepso -->
+                    <div>
+                        <!-- input form -->
+                        <!-- ide kell a kozepso div címe, kozepre igazítva-->
+                        <div class=" input-group input-group-sm mb-3 mid inputField">
+                            <span class="input-group-text" id="inputGroup-sizing-s">Vezetéknév</span> <!-- ID ne felejtsd -->
+                            <input type="text" class="form-control mid" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-s">
                         </div>
+                        <div class="input-group input-group-sm mb-3 inputField">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">Keresztnév</span>
+                            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                        </div>
+                        <!-- dropdown -->
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown button
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                                <li><a class="dropdown-item active" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            </ul>
+                        </div>
+                        <!-- dropdown -->
+                        <div class="dropdown">
+                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+                                <span class="caret"></span></button>
+
+                            <div class="dropdown-menu">
+                                <li><a class="dropdown-item">HTML</a></li>
+                                <li><a class="dropdown-item">HTML</a></li>
+                                <li><a class="dropdown-item">HTML</a></li>
+                            </div>
+                        </div>
+
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown button
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+
+
+                        <br> - <br>
+
+                        <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-macos mx-0 border-0 shadow" style="width: 220px;">
+                            <li><a class="dropdown-item active" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        </ul>
+
+                        <div>
+                            <div class="dropdown">
+                                <a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none dropdown-toggle" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <svg class="bi me-2" width="40" height="32">
+                                        <use xlink:href="#bootstrap"></use>
+                                    </svg>
+                                </a>
+                                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownNavLink" style="width: 220px;">
+                                    <li><a class="dropdown-item active" href="#" aria-current="page">Overview</a></li>
+                                    <li><a class="dropdown-item" href="#">Inventory</a></li>
+                                    <li><a class="dropdown-item" href="#">Customers</a></li>
+                                    <li><a class="dropdown-item" href="#">Products</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Reports</a></li>
+                                    <li><a class="dropdown-item" href="#">Analytics</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <button id="createWorkerBtn" type="button" class="btn btn-primary">Mentés</button>
                     </div>
+
                 </div>
+
             </div>
+
         </div>
-    </body>
+
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+            <li><a class="dropdown-item active" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Separated link</a></li>
+        </ul>
+    </div>
 </body>
 
 </html>
