38efd4d445a4a956562b808b808e575847919322
guttidani <61143051+guttidani@users.noreply.github.com>
2022-04-26 03:47:22 +0200

    fixing on linux

--- body.php
+++ body.php
@@ -5,6 +5,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script defer src="js/validation.js"></script>
 </head>
 
 
